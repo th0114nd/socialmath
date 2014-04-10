9f7c96732de19ec667ed785645cbeab37da963e2
Kexin Rong <kexinrongg@gmail.com>
2014-04-09 18:16:12 -0700

    Schema change; add managers - Combine Proof class into Node class. Node class now supports type   theorem, axiom, definition and proof. - Add managers for Node class and Keyword class.

--- socialmath/prooftree/admin.py
+++ socialmath/prooftree/admin.py
@@ -3,8 +3,6 @@ from prooftree.models import *
 
 # Register your models here.
 admin.site.register(Node)
-admin.site.register(Proof)
-admin.site.register(Prove)
 admin.site.register(DAG)
 admin.site.register(Keyword)
 admin.site.register(KWMap)
--- socialmath/prooftree/models.py
+++ socialmath/prooftree/models.py
@@ -3,26 +3,17 @@ from django.db import models
 ''' Create the following tables:
 CREATE TABLE "prooftree_node" (
     "node_id" integer NOT NULL PRIMARY KEY,
-    "type" integer NOT NULL,
-    "title" varchar(100) NOT NULL,
-    "statement" text NOT NULL
-)
-;
-CREATE TABLE "prooftree_proof" (
-    "pf_id" integer NOT NULL PRIMARY KEY,
-    "content" text NOT NULL
-)
-;
-CREATE TABLE "prooftree_prove" (
-    "id" integer NOT NULL PRIMARY KEY,
-    "node_id" integer NOT NULL REFERENCES "prooftree_node" ("node_id"),
-    "pf_id" integer NOT NULL REFERENCES "prooftree_proof" ("pf_id")
+    "ntype" varchar(3) NOT NULL,
+    "title" varchar(100),
+    "statement" text NOT NULL,
+    "pub_time" datetime NOT NULL
 )
 ;
 CREATE TABLE "prooftree_dag" (
     "id" integer NOT NULL PRIMARY KEY,
     "parent_id" integer NOT NULL REFERENCES "prooftree_node" ("node_id"),
-    "child_id" integer NOT NULL REFERENCES "prooftree_node" ("node_id")
+    "child_id" integer NOT NULL REFERENCES "prooftree_node" ("node_id"),
+    "dep_type" varchar(3) NOT NULL
 )
 ;
 CREATE TABLE "prooftree_keyword" (
@@ -38,41 +29,80 @@ CREATE TABLE "prooftree_kwmap" (
 ;
 
 '''
-# Entity for a node in DAG. A node can be a theorem (type = 0).
-# a definition (type = 1).
+
+# Custom manager for Node class
+class NodeManager(models.Manager):
+    # Usage: DAG.objects.get_children(parent_id)
+    # Return: QuerySet
+    def get_children(self, parent_id):
+        return self.filter(node_id=parent_id).values('child_id')
+
+    # Usage: DAG.objects.get_parents(child_id)
+    # Return: QuerySet
+    def get_parents(self, child_id):
+        return self.filter(node_id=child_id).values('parent_id')
+
+# Custom manager for Keyword class
+class KWManager(models.Manager):
+    # Usage: Keyword.objects.get_related(keyword)
+    # Return: QuerySet of node_id for theorems tagged with keyword
+    def get_related(self, keyword):
+        kw_id = self.get(word__exact=Keyword).values()[0]['kw_id']
+        return KWMap.objects.filter(kw_id=kw_id).values('node_id')
+
+    # Usage: Keyword.objects.get_keywords(node_id)
+    # Return: QuerySet of kw_id for theorems tagged with keyword
+    def get_keywords(self, node_id):
+        kw = KWMap.objects.filter(node_id=node_id).values('kw_id')
+        kw_list = []
+        for item in kw:
+            kw_list.append(item['kw_id'])
+        return self.filter(kw_id_in=kw_list).values('word')
+
+# Entity for a node in DAG. 
 class Node(models.Model):
-	node_id = models.AutoField(primary_key=True)
-	type = models.IntegerField()
-	title = models.CharField(max_length=100)
-	statement = models.TextField()
-	def __str__(self):
-		return self.title
+    TYPES = (
+        ('thm', 'Theorem'),
+        ('ax', 'Axiom'),
+        ('def', 'Definition'),
+        ('pf', 'Proof'),
+    )
+    node_id = models.AutoField(primary_key=True)
+    ntype = models.CharField(max_length=3, choices=TYPES)
+    title = models.CharField(max_length=100, null=True)
+    statement = models.TextField()
+    pub_time = models.DateTimeField()
+    objects = NodeManager() 
 
-# Entity for proofs.
-class Proof(models.Model):
-	pf_id = models.AutoField(primary_key=True)
-	title = models.CharField(max_length=100)
-	content = models.TextField()
-	def __str__(self):
-		return self.title
-
-# Mapping between theorem and proofs
-class Prove(models.Model):
-	node = models.ForeignKey(Node)
-	pf = models.ForeignKey(Proof)
+    def __str__(self):
+        return self.title
 
 # Adjacency list of nodes in the DAG
 class DAG(models.Model):
-	parent = models.ForeignKey(Node, related_name="parent_id")
-	child = models.ForeignKey(Node, related_name="child_id")
+    TYPES = (
+        ('any', 'any'),
+        ('all', 'all'),
+    )
+    parent = models.ForeignKey(Node, related_name="parent_id")
+    child = models.ForeignKey(Node, related_name="child_id")
+    dep_type = models.CharField(max_length=3, choices=TYPES)
 
 # Entity for keywords
 class Keyword(models.Model):
-	kw_id = models.AutoField(primary_key=True)
-	word = models.CharField(max_length=100)
+    kw_id = models.AutoField(primary_key=True)
+    word = models.CharField(max_length=100)
+    objects = KWManager() 
 
-# Mapping between nodes and keywords
+# Many to many mapping between nodes and keywords
 class KWMap(models.Model):
 	node = models.ForeignKey(Node)
 	kw = models.ForeignKey(Keyword)
 
+
+# TODOs:
+# - Node, proof + pub time
+# - Proof -> theorem
+# - Get all keywords mapped to a theorem
+# - Search theorems by keywords
+# - Get all proofs of a theorem
+# - Get all children to a node
