040416201d38f6c89d5b376a49c26fd49aa98ab0
Jianchi Chen <jchen2@caltech.edu>
2014-04-11 02:18:04 -0700

    Added index page template and url specifier; - Will work on completing the templates and view tomorrow

--- socialmath/prooftree/views.py
+++ socialmath/prooftree/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.forms import ModelForm
 from prooftree.models import *
+from django.http import HttpResponse
 
 # Create your views here.
 class NodeForm(ModelForm):
@@ -34,3 +35,8 @@ def node_delete(request, id, template_name='prooftree/node_delete.html'):
 		node.delete()
 		return redirect('node_list')
 	return render(request, template_name, {'object':node})
+
+def index(request):
+    latest_theorem_list = Node.objects.all().order_by('-pub_time')[:10]
+    context = {'latest_theorem_list': latest_theorem_list}
+    return render(request, 'prooftree/index.html', context)
