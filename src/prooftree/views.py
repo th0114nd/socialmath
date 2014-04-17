712a5f220f6e8933bfc9694b69b9f30751fe6e98
Kexin Rong <kexinrongg@gmail.com>
2014-04-17 15:40:11 -0700

    Bug fix for view.detail view.detail should work now. The function returns a json in the format specified by the api.

--- src/prooftree/views.py
+++ src/prooftree/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from prooftree.models import *
 from prooftree.serializers import *
+from django.shortcuts import render, redirect, get_object_or_404
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
 class JSONResponse(HttpResponse):
     """
@@ -12,9 +15,11 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 def index(request):
-    return
+    latest_theorem_list = Node.objects.all().order_by('-pub_time')[:10]
+    context = {'latest_theorem_list': latest_theorem_list}
+    return render(request, 'prooftree/index.html', context)
 
-def pagebrief(request, pageno=1):
+def pagebrief(request, pageno='1'):
     ''' **HTTP GET**
         /get/brief
         /get/brief?page=n
@@ -22,6 +27,7 @@ def pagebrief(request, pageno=1):
     if request.method == 'GET':
         # Check whether page number is valid
         max_pageno = Node.objects.max_pageno()
+        pageno = int(pageno)
         if pageno > max_pageno:
             return HttpResponse(status=404)
         # Get serialized data for each node on the page
@@ -42,7 +48,7 @@ def pagebrief(request, pageno=1):
         return JSONResponse(response)
 
 
-def pagemedium(request, pageno=1):
+def pagemedium(request, pageno='1'):
     ''' **HTTP GET**
         /get/medium
         /get/medium?page=n
@@ -50,6 +56,7 @@ def pagemedium(request, pageno=1):
     if request.method == 'GET':
         # Check whether page number is valid
         max_pageno = Node.objects.max_pageno()
+        pageno = int(pageno)
         if pageno > max_pageno:
             return HttpResponse(status=404)
         # Get serialized data for each node on the page
@@ -74,22 +81,25 @@ def detail(request, node_id):
         /get/one/<id>
     '''
     # Error handling
-    if node_id == NULL:
+    maxid = Node.objects.max_id()
+    node_id = int(node_id)
+    if node_id is None:
         return HttpResponse("Bad Request. An id is required.", status=400)
-    if node_id > Node.objects.max_id():
-            return HttpResponse("Bad Request. Id not found.", status=404)
+    if int(node_id) > int(maxid):
+        return HttpResponse("Bad Request. Id not found.", status=404)
     try:
         node = Node.objects.get(node_id=node_id)
     except Node.ObjectDoesNotExist:
         return HttpResponse("Bad Request. Id has been delete.", status=410)
 
     if request.method == 'GET':
-        neighbor_ids = DAG.objects.get_children(node_id) + DAG.objects.get_parents(node_id)
+        neighbor_ids = list(DAG.objects.get_children(node_id)) + \
+                       list(DAG.objects.get_parents(node_id))
         neighbors = []
         for neighbor_id in neighbor_ids:
             neighbor = Node.objects.get(node_id=neighbor_id)
             serializer = NodeSerializer(neighbor)
-            neighbours.append(serializer.data)
+            neighbors.append(serializer.data)
 
         # Form responses
         response = {'wanted': NodeSerializer(node).data, 'neighbors':neighbors}
