f0fc62c1b08a06ddc07dcf99cf1718bf048e191b
Tim Holland <th0114nd@gmail.com>
2014-04-18 03:32:54 -0700

    Created settings for deployment

--- src/socialmath/settings.py
+++ src/socialmath/settings.py
@@ -60,11 +60,11 @@ WSGI_APPLICATION = 'socialmath.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mydb',
-        'USER': 'djangouser',
-        'PASSWORD': 'mypassword',
-        'HOST': '',
-        'PORT': ''
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
 
